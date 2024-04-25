535b212a7e79dbaa829f8393d13f73f451880cb3
许立勣 <14340890+xu-liji1031@user.noreply.gitee.com>
2024-04-25 17:44:07 +0800

    add sock_client.c

--- sock_client.c
+++ sock_client.c
@@ -35,7 +35,8 @@
 #include "sock_sqlite.h"
 #include "temp.h"
 #include "packet.h"
-
+#include "logger.h"
+#include "proc.h"
 
 int check_sample_time(time_t *last_time,int timeout);
 
@@ -47,6 +48,7 @@ static inline void print_usage(char *progname)
 	printf("-i[IP Address]\n");
 	printf("-p[Port]\n");
 	printf("-t[timeout]\n");
+	printf("-d[debug]\n");
 	return ;
 }
 
@@ -74,9 +76,7 @@ static inline void msleep(unsigned long ms)
 
 int main(int argc, char **argv)
 {
-
-
-	int					timeout = 60;
+	int					timeout;
 	int					opt;
 	char				*progname = NULL;
 	int					rv = -1;
@@ -84,7 +84,7 @@ int main(int argc, char **argv)
 	float				temp;
 	char				name_buf[64];
 	int					flag;
-	time_t				now_time,last_time;
+	time_t				last_time = 0;
 	struct tcp_info		optval;
 	socklen_t 			optlen = sizeof(optval);
 	int					ret;
@@ -95,21 +95,26 @@ int main(int argc, char **argv)
 	data_t				data;
 	socket_t 			my_socket;
 	char				*send_buf;
-	progname = (char *)basename( argv[0] );
+	char                *logfile="sock_client.log";
+	int                 loglevel=LOG_LEVEL_INFO;
+	int                 logsize=10; /*  logfile size max to 10K */ 
+	int					daemon = 1;
+	int					bytes;	
 
-
+	progname =		    (char *)basename(argv[0]);
 	struct option		long_options[] = 
 	{
 		{"port", required_argument, NULL, 'p'},
 		{"IP Address", required_argument, NULL, 'i'},
 		{"timeout", required_argument, NULL, 't'},
 		{"help", no_argument, NULL, 'h'},
+		{"debug", no_argument, NULL, 'd'},
 		{NULL, 0, NULL, 0}
 
 	};
 
 
-	while( (opt = getopt_long(argc,argv,"p:i:t:h",long_options,NULL)) != -1 )
+	while( (opt = getopt_long(argc,argv,"p:i:t:hd",long_options,NULL)) != -1 )
 	{
 		switch(opt)
 		{
@@ -122,6 +127,10 @@ int main(int argc, char **argv)
 			case 't':
 				timeout = atoi(optarg);
 				break;
+			case 'd':
+				daemon = 0;
+				logfile="console";/* set log_name:console*/
+				loglevel=LOG_LEVEL_DEBUG;/*set level is record all debug information*/
 			case 'h':
 				print_usage(progname);
 				return 0;
@@ -131,62 +140,77 @@ int main(int argc, char **argv)
 	}
 
 
-	open_sqlite3();
+	if( log_open(logfile, loglevel, logsize, THREAD_LOCK_NONE) < 0 )
+	{
+		fprintf(stderr, "Initial log system failed\n");
+		return 1;
+	}
 
-//	last_time = time(NULL);
+	rv = open_sqlite3();
 
-	while(1)
+	if( 0 == rv )
+	{
+		log_debug("Create table ok\n");
+	}
+	else
+	{
+		log_error("Create table error:%s\n",strerror(errno));
+	}
+	
+	install_default_signal();	
+
+
+	while( ! g_signal.stop )
 	{
 		flag = 0;
 
 		if( check_sample_time(&last_time, timeout) )
 		{
-			printf("------------------Start get data--------------------\n");
-			if( get_temperature(&data.d_temp) == 0 )
+			log_debug("------------------Start get data--------------------\n");
+			if( get_temperature( &data.d_temp ) == 0 )
 			{
-				printf("Temperature:%.2f\n",data.d_temp);
+				log_info("Temperature:%.2f\n", data.d_temp);
 			}
 			else
 			{
-				printf("Temperature error:%s\n",strerror(errno));
+				log_error("Temperature error:%s\n", strerror(errno));
 			}
-			if( (get_name(data.d_name,sizeof(data.d_name))) != 0 )
+			if( (get_name(data.d_name, sizeof(data.d_name))) != 0 )
 			{
-				printf("Get name error:%s\n",strerror(errno));
+				log_error("Get name error:%s\n", strerror(errno));
 			}
 			else
 			{
-				printf("name:%s\n",data.d_name);
+				log_info("name:%s\n", data.d_name);
 			}
 			data.d_time = get_time(NULL);
 			if( data.d_time  != NULL )
 			{
-				printf("Current time is:%s\n",data.d_time);
+				log_info("Current time is:%s\n", data.d_time);
 			}
 			else
 			{
-				printf("Get current time failure:%s\n",strerror(errno));
+				log_error("Get current time failure:%s\n", strerror(errno));
 			}
 
-
-			last_time = now_time;
+			log_debug("Get the important data ok\n");
 			flag = 1;
 		}
-		sleep(2);
 
-
+				
 		ret = getsockopt(my_socket.conn_fd, IPPROTO_TCP, TCP_INFO, &optval, &optlen);
-	
+			
 
 		if( ret != 0 )
 		{
-			rv = sock_connect(&my_socket);
+			rv = sock_connect( &my_socket );
+			
 			if( rv < 0 )
 			{
-				close(my_socket.conn_fd);
 
 				if( flag == 1 )
 				{
+					log_debug("Start to insert_data\n");
 					insert_data(data);
 
 				}
@@ -199,9 +223,8 @@ int main(int argc, char **argv)
 		if( flag == 1 )
 		{
 			/* 发送当前采样的数据*/
-			rv1 = sock_write(&my_socket,data,snd_buf);
-			printf("Send the data ok\n");
-			//internet_read(&my_socket);
+			rv1 = sock_write(&my_socket, data, snd_buf);
+			log_info("Send the data ok\n");
 
 
 			/* 发送失败，将数据再存到数据库中*/
@@ -221,19 +244,29 @@ int main(int argc, char **argv)
 			send_buf = read_data();
 			
 			if(send_buf != NULL)
-				printf("Extract data ok\n");
+				log_info("Extract data ok\n");
 			else
 			{
-				printf("Error:%s\n",strerror(errno));
+				log_error("Error:%s\n", strerror(errno));
 			}
 			
-			send_data(send_buf,&my_socket);
+			rv = send_data(send_buf, &my_socket);
+			if( rv < 0 )
+			{
+				log_error("Send error\n");
+			}
 			
 			/* 删除一条数据*/
-			del_database(snd_buf);
+			rv = del_database(snd_buf);
+			if( rv < 0 )
+			{
+				log_error("delete the data error:%s\n",strerror(errno));
+			}
+
 		}
-		msleep(5);
 	}
+	
+	msleep(5);
 	close_database();
 	return 0;
 }
@@ -241,7 +274,7 @@ int main(int argc, char **argv)
 
 
 
-int check_sample_time(time_t *last_time,int timeout)
+int check_sample_time(time_t *last_time, int timeout)
 {
 	int 		time_sample = 0;
 	time_t		now;
